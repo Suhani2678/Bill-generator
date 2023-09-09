5a95606c27f6d82cf8a1dbb1c57dc759ab6d4dd5
Suhani Gupta <105312111+Suhani2678@users.noreply.github.com>
2023-09-09 16:40:17 +0530

    Create DeluxePizza.java

---
+++ DeluxePizza.java
@@ -0,0 +1,12 @@
+
+public class DeluxePizza extends Pizza{
+	public 	DeluxePizza(Boolean veg) {
+		super(veg);
+		//super.addextracheese();
+		//super.addextratoppings();
+		//super.takeaway();
+	}
+@Override
+public void addextracheese() {}
+public void addextratoppings() {}
+}
